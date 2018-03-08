1d2834ba7cf3b21c3334be5ec878e2ed34708358
EnriqueMB <kikeballina1@gmail.com>
2018-03-08 16:03:05 -0600

    MARCA VEHICULO

--- CreativaSL.Web.Ganado/Models/CatMarcaVehiculoModels.cs
+++ CreativaSL.Web.Ganado/Models/CatMarcaVehiculoModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,14 @@ namespace CreativaSL.Web.Ganado.Models
 {
     public class CatMarcaVehiculoModels
     {
+        public CatMarcaVehiculoModels()
+        {
+            _Descripcion = string.Empty;
+            Conexion = string.Empty;
+            Usuario = string.Empty;
+            _ListaMarcas = new List<CatMarcaVehiculoModels>();
+        }
+
         private int _IDMarca;
 
         public int IDMarca
@@ -16,12 +25,24 @@ namespace CreativaSL.Web.Ganado.Models
         }
 
         private string _Descripcion;
-
+        [Required(ErrorMessage = "La marca es obligatorio")]
+        [Display(Name = "marca")]
+        [StringLength(80, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo de {1}.", MinimumLength = 1)]
+        [RegularExpression(@"^[A-Za-záéíóúñÁÉÍÓÚÑ0-9\(\)\-\,\.\;\:\s]*$", ErrorMessage = "Solo Letras y números")]
         public string Descripcion
         {
             get { return _Descripcion; }
             set { _Descripcion = value; }
         }
+
+        private List<CatMarcaVehiculoModels> _ListaMarcas;
+
+        public List<CatMarcaVehiculoModels> ListaMarcas
+        {
+            get { return _ListaMarcas; }
+            set { _ListaMarcas = value; }
+        }
+
 
         #region Datos De Control
         public string Conexion { get; set; }
