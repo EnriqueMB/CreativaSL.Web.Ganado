a56af7dbf6e150a4c9dec5ef9c691ef1b367eb55
Eder Ramírez <eder-ramtz@hotmail.com>
2018-03-12 11:40:53 -0600

    x

--- CreativaSL.Web.Ganado/Models/CatTipoVehiculoModels.cs
+++ CreativaSL.Web.Ganado/Models/CatTipoVehiculoModels.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace CreativaSL.Web.Ganado.Models
 {
     public class CatTipoVehiculoModels
+        
     {
+        public CatTipoVehiculoModels() {
+            _IDTipoVehiculo = 0;
+            _Descripcion = string.Empty;
+            Conexion = string.Empty;
+            Resultado = 0;
+            Completado = false;
+            Usuario = string.Empty;
+            Opcion = 0;
+        }
+        private List<CatTipoVehiculoModels> _listaTipoVehiculos;
+
+        public List<CatTipoVehiculoModels> listaTipoVehiculos
+        {
+            get { return _listaTipoVehiculos; }
+            set { _listaTipoVehiculos = value; }
+        }
+
+
         private int _IDTipoVehiculo;
 
         public int IDTipoVehiculo
@@ -16,7 +36,10 @@ namespace CreativaSL.Web.Ganado.Models
         }
 
         private string _Descripcion;
-
+        [Required(ErrorMessage = "El tipo de vehículo es obligatorio")]
+        [Display(Name = " tipo de vehículo")]
+        [StringLength(80, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo de {1}.", MinimumLength = 1)]
+        [RegularExpression(@"^[A-Za-záéíóúñÁÉÍÓÚÑ0-9\(\)\-\,\.\;\:\s]*$", ErrorMessage = "Solo Letras y números")]
         public string Descripcion
         {
             get { return _Descripcion; }
